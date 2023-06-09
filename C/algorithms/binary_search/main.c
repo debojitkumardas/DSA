4479880d829bebcf374afb5cf0d25983a674187d
DebojitKumarDas <debojitkumardas@localhost.localdomain>
2023-06-09 18:55:00 +0530

    fixed an error

--- C/algorithms/binary_search/main.c
+++ C/algorithms/binary_search/main.c
@@ -9,25 +9,25 @@ int main(void) {
     for (int i = 0; i < 8; i++)
         ar[i] = i + 1;
 
-    binary_search(ar, 7, 7, 0);
+    binary_search(ar, 0, 0, 7);
 
     return 0;
 }
 
-void binary_search(int ar[], int n, int x, int start) {
+void binary_search(int ar[], int x, int s, int e) {
 
-    if (n >= start) {
-        int mid = start + (n - start) / 2;
+    if (e >= s) {
+        int mid = s + (e - s) / 2;
 
         if (ar[mid] == x) {
-            printf("found the element %d at %d\n", ar[mid], mid);
+            printf("Found the element %d at position %d\n", ar[mid], mid);
             return;
         }
 
-        if (ar[mid] < x)
-            binary_search(ar, n, x, mid + 1);
+        if (ar[mid] > x)
+            binary_search(ar, x, s, mid-1);
         else
-            binary_search(ar, mid, x, start);
+            binary_search(ar, x, mid+1, e);
     }
 
     else
