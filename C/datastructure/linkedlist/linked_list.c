e2a0f42e06ca5627779b847fab55229d6d6ca5a8
Debojit Kumar Das <debojit2997@gmail.com>
2023-06-13 20:31:44 +0530

    added code for selection sort

--- C/datastructure/linkedlist/linked_list.c
+++ C/datastructure/linkedlist/linked_list.c
@@ -1,7 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node *link;
+// self referential structure
+struct node {
+    int item;  // each node contains an integer
+    struct node* link;  // pointer to the next node
+};  // end structure node
+
+typedef struct node node;
+
+// function prototypes
+void insert (node* ptr, int val);  // insert value at the end of list
+char delete (node* ptr, int pos);  // delete value at any position in list
+int is_empty (node* ptr);  // check if list is empty
+void print_list (node* ptr);  // print list
+void instructions (void);  // has instructions on insetion, deletion of list elements
+
+int main (void) {
+
+    node* head = NULL;  // initially there are no nodes
+    int choice, value, position;  // users choice, value and position
+
+    instructions();
+    printf("? ");
+    scanf("%d", &choice);
+
+    // while user doesn't choose 3
+    while (choice != 3) {
+
+        switch (choice) {
+            case 1:
+                printf("enter a value: ");
+                scanf("%d", &value);
+                insert(head, value);
+                print_list(head);
+                break;
+            case 2:
+                // if list is not empty
+                if (!is_empty(head)) {
+                    printf("enter the postion of the element to be deleted: ");
+                    scanf("%d", &position);
+                }
+        }
+    }
+}
+
+
+
+/*
+typedef struct node* link;
 
 struct node {
     int item;
@@ -36,3 +83,4 @@ int main() {
 
     return 0;
 }
+*/
