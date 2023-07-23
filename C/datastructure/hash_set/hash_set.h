aea57125cef08e5b0e735d0d1fd3740ba2faa6b0
Debojit Kumar Das <debojit2997@gmail.com>
2023-07-23 15:51:22 +0530

    hash_set code

--- C/datastructure/hash_set/hash_set.h
+++ C/datastructure/hash_set/hash_set.h
@@ -1,4 +1,31 @@
 #ifndef HASH_SET_H
 #define HASH_SET_H
 
+#define SET_SIZE 3
+
+typedef enum {FALSE, TRUE} bool_;
+
+typedef struct node node;
+typedef struct hash_set hash_set;
+
+struct node {
+    int num;
+    node *next;
+};
+
+bool_ is_empty(node *head);
+void insert_node_at_tail(node **head, node **tail, int num);
+void remove_node(node **head, node **tail, int num);
+
+struct hash_set {
+    node* set_list[SET_SIZE];
+};
+
+int hash_func(int key);
+bool_ contains(hash_set *hash_set_head, int key);
+void insert_data(hash_set *hash_set_head, hash_set *hash_set_tail, int key);
+void remove_data(hash_set *hash_set_head, hash_set *hash_set_tail, int key);
+void print_set(hash_set *hash_set_head);
+void delete_set(hash_set *hash_set_head);
+
 #endif // !HASH_SET_H
