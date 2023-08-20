a2658fc14a0d3ddb6faadc948750b102436cfb7d
Debojit Kumar Das <debojit2997@gmail.com>
2023-08-20 15:45:19 +0530

    Added code for topological sort

---
+++ C/algorithms/Top_sort/hdr/graph_ds.h
@@ -0,0 +1,19 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include "stack_ds.h"
+
+typedef struct Graph Graph;
+
+struct Graph {
+    int num_vertices;
+    int* visited;
+    Node** adj_list;
+};
+
+Graph* CreateGraph(int vertices);
+void AddEdge(Graph** graph, int src, int dest);
+void PrintGraph(Graph* graph);
+void DeleteGraph(Graph** graph);
+
+#endif // !GRAPH_H
---
+++ C/algorithms/Top_sort/src/main.c
@@ -0,0 +1,88 @@
+#include "../hdr/graph_ds.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#define CTOI(ch) (int)ch - (int)'a'
+#define ITOC(in) (char)(in + (int)'a')
+
+int DFSIter(Graph* graph, int start, int* order, int index) {
+    int current;
+    Node* head = NULL;
+
+    PushNode(&head, start);
+
+    while (head != NULL) {
+        current = head->value;
+        PopNode(&head);
+
+        if (graph->visited[current] == 1)
+            continue;
+        graph->visited[current] = 1;
+
+        order[index] = current;
+        --index;
+
+        Node* neighbour = graph->adj_list[current];
+        while (neighbour != NULL) {
+            PushNode(&head, neighbour->value);
+            neighbour = neighbour->next;
+        }
+    }
+
+    DeleteStack(&head);
+
+    return index;
+}
+
+int* TopologicalSort(Graph* graph) {
+    int vertices = graph->num_vertices;
+    int index = vertices - 1;
+    int* order = malloc(vertices * sizeof(int));
+
+    for (int i = 0; i < vertices; ++i) {
+        if (graph->visited[i] == 0) {
+            index = DFSIter(graph, i, order, index);
+        }
+    }
+
+    return order;
+}
+
+int main(void) {
+    int vertices = 13;
+    Graph* graph = CreateGraph(vertices);
+
+    AddEdge(&graph, CTOI('a'), CTOI('d'));
+    AddEdge(&graph, CTOI('b'), CTOI('d'));
+    AddEdge(&graph, CTOI('c'), CTOI('a'));
+    AddEdge(&graph, CTOI('c'), CTOI('b'));
+    AddEdge(&graph, CTOI('d'), CTOI('g'));
+    AddEdge(&graph, CTOI('d'), CTOI('h'));
+    AddEdge(&graph, CTOI('e'), CTOI('a'));
+    AddEdge(&graph, CTOI('e'), CTOI('d'));
+    AddEdge(&graph, CTOI('e'), CTOI('f'));
+    AddEdge(&graph, CTOI('f'), CTOI('j'));
+    AddEdge(&graph, CTOI('f'), CTOI('k'));
+    AddEdge(&graph, CTOI('g'), CTOI('i'));
+    AddEdge(&graph, CTOI('h'), CTOI('i'));
+    AddEdge(&graph, CTOI('h'), CTOI('j'));
+    AddEdge(&graph, CTOI('i'), CTOI('l'));
+    AddEdge(&graph, CTOI('j'), CTOI('l'));
+    AddEdge(&graph, CTOI('j'), CTOI('m'));
+    AddEdge(&graph, CTOI('k'), CTOI('j'));
+
+    PrintGraph(graph);
+
+    int* order = TopologicalSort(graph);
+
+    if (order != NULL) {
+        for (int v = 0; v < vertices; ++v)
+            printf("%c ", ITOC(order[v]));
+        printf("\b\b\n");
+    }
+
+    free(order);
+    DeleteGraph(&graph);
+
+    return 0;
+}
