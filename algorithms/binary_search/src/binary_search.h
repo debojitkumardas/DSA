b0ee936c7cce660c909f62d09de471205c8ccbc6
Debojit Kumar Das <debojit2997@gmail.com>
2024-06-19 22:53:40 +0530

    Correction to binary search code

--- algorithms/binary_search/src/binary_search.h
+++ algorithms/binary_search/src/binary_search.h
@@ -4,16 +4,16 @@
 #include <cstddef>
 
 template <typename T>
-T BinarySearch(T* arr, size_t len, T key, char sort = 'a') {
+size_t BinarySearch(T* arr, size_t len, T key, char sort = 'a') {
     size_t left_index = 0;
     size_t right_index = len - 1;
-    size_t mid = left_index + (right_index - left_index + 1) / 2;
+    size_t mid = left_index + (right_index - left_index) / 2;
 
     if (sort == 'a') {
-        while (left_index < right_index && right_index > 0) {
+        while (left_index <= right_index) {
             if (arr[mid] == key) {
-                return arr[mid];
-            } else if (arr[mid] > key) {
+                return mid;
+            } else if (arr[mid] > key && mid > 0) {
                 right_index = mid - 1;
             } else {
                 left_index = mid + 1;
@@ -21,10 +21,10 @@ T BinarySearch(T* arr, size_t len, T key, char sort = 'a') {
             mid = left_index + (right_index - left_index) / 2;
         }
     } else {
-        while (left_index < right_index && right_index > 0) {
+        while (left_index <= right_index) {
             if (arr[mid] == key) {
-                return arr[mid];
-            } else if (arr[mid] < key) {
+                return mid;
+            } else if (arr[mid] < key && mid > 0) {
                 right_index = mid - 1;
             } else {
                 left_index = mid + 1;
@@ -33,7 +33,7 @@ T BinarySearch(T* arr, size_t len, T key, char sort = 'a') {
         }
     }
 
-    return {};
+    return -1;
 }
 
 #endif // !BINARY_SEARCH_H
